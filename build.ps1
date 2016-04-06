14f2604675b71e58811affd099e8c7990dd7d6f6
Andrew Jackura <ajackura@google.com>
2016-04-06 12:26:01 -0400

    fix powershell build script

--- build.ps1
+++ build.ps1
@@ -1,5 +1,5 @@
-if (-not gc googet.goospec | where {$_ -match '"version": "(.+)",'}) {
-  throw 'Error grabbing version from goospec"
+if (!(gc googet.goospec | where {$_ -match '"version": "(.+)",'})) {
+  throw 'Error grabbing version from goospec'
 }
 $version = $matches[1]
 
