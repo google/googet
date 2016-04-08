1d9a7bbe0c0fed2c932f75a8f9922f9012012d6a
Andrew Jackura <ajackura@google.com>
2016-04-08 15:05:57 -0400

    add test

--- oswrap/oswrap_windows.go
+++ oswrap/oswrap_windows.go
@@ -1,8 +1,18 @@
 //+build windows
 
-// Package oswrap exists to translate pathnames into extended-length path names
-// behind the scenes, so that googet can install packages with deep directory
-// structures
+/*
+Copyright 2016 Google Inc. All Rights Reserved.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package oswrap
 
 import (
@@ -82,25 +92,17 @@ func Mkdir(name string, mode os.FileMode) error {
 
 // MkdirAll calls os.MkdirAll with name normalized
 func MkdirAll(name string, mode os.FileMode) error {
-	// os.MkdirAll does not work with extended-length paths if
-	// nothing in the path exists.
-	vol := filepath.VolumeName(name)
-	i := len(vol) + 1
-	for !os.IsPathSeparator(name[i]) {
-		i++
-	}
-	baseDir := name[:i]
-
-	if _, err := Stat(baseDir); err != nil {
-		if err := Mkdir(baseDir, mode); err != nil {
-			return err
-		}
-	}
-
 	name, err := normPath(name)
 	if err != nil {
 		return err
 	}
+
+	// os.MkdirAll does not work with extended-length paths if
+	// nothing in the path exists.
+	if err := mkRootDir(name, mode); err != nil {
+		return err
+	}
+
 	return os.MkdirAll(name, mode)
 }
 
