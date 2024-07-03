d3903d812393f45f58d1791f79239db26daffed1
Phillip Nguyen <nguyenphillip@google.com>
2024-07-03 15:05:39 +0000

    Check for same version already installed in updates code.
    If the already installed version is the exact same as the version
    available in a higher priority repo, then there is no reason to
    reinstall that version. Also added tests for the updates code.

--- googet_test.go
+++ googet_test.go
@@ -391,3 +391,82 @@ func TestCleanPackages(t *testing.T) {
 		t.Errorf("cleanPackages did not remove notWantDir")
 	}
 }
+
+func TestUpdates(t *testing.T) {
+	for _, tc := range []struct {
+		name string
+		pm   packageMap
+		rm   client.RepoMap
+		want []goolib.PackageInfo
+	}{
+		{
+			name: "upgrade to later version",
+			pm: packageMap{
+				"foo.x86_32": "1.0",
+				"bar.x86_32": "2.0",
+			},
+			rm: client.RepoMap{
+				"stable": client.Repo{
+					Priority: 500,
+					Packages: []goolib.RepoSpec{
+						{PackageSpec: &goolib.PkgSpec{Name: "foo", Version: "2.0", Arch: "x86_32"}},
+						{PackageSpec: &goolib.PkgSpec{Name: "bar", Version: "2.0", Arch: "x86_32"}},
+					},
+				},
+			},
+			want: []goolib.PackageInfo{{Name: "foo", Arch: "x86_32", Ver: "2.0"}},
+		},
+		{
+			name: "rollback to earlier version",
+			pm: packageMap{
+				"foo.x86_32": "2.0",
+				"bar.x86_32": "2.0",
+			},
+			rm: client.RepoMap{
+				"stable": client.Repo{
+					Priority: 500,
+					Packages: []goolib.RepoSpec{
+						{PackageSpec: &goolib.PkgSpec{Name: "foo", Version: "2.0", Arch: "x86_32"}},
+						{PackageSpec: &goolib.PkgSpec{Name: "bar", Version: "2.0", Arch: "x86_32"}},
+					},
+				},
+				"rollback": client.Repo{
+					Priority: 1500,
+					Packages: []goolib.RepoSpec{
+						{PackageSpec: &goolib.PkgSpec{Name: "foo", Version: "1.0", Arch: "x86_32"}},
+					},
+				},
+			},
+			want: []goolib.PackageInfo{{Name: "foo", Arch: "x86_32", Ver: "1.0"}},
+		},
+		{
+			name: "no change if rollback version already installed",
+			pm: packageMap{
+				"foo.x86_32": "1.0",
+			},
+			rm: client.RepoMap{
+				"stable": client.Repo{
+					Priority: 500,
+					Packages: []goolib.RepoSpec{
+						{PackageSpec: &goolib.PkgSpec{Name: "foo", Version: "2.0", Arch: "x86_32"}},
+						{PackageSpec: &goolib.PkgSpec{Name: "bar", Version: "2.0", Arch: "x86_32"}},
+					},
+				},
+				"rollback": client.Repo{
+					Priority: 1500,
+					Packages: []goolib.RepoSpec{
+						{PackageSpec: &goolib.PkgSpec{Name: "foo", Version: "1.0", Arch: "x86_32"}},
+					},
+				},
+			},
+			want: nil,
+		},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+			pi := updates(tc.pm, tc.rm)
+			if diff := cmp.Diff(tc.want, pi); diff != "" {
+				t.Errorf("update(%v, %v) got unexpected diff (-want +got):\n%v", tc.pm, tc.rm, diff)
+			}
+		})
+	}
+}
