21a19e7e30aabdcfe04a595583bf0243a644d1b8
Steve Brito <stevebrito@google.com>
2025-05-19 07:05:25 -0700

    fix system linux

--- system/system_linux.go
+++ system/system_linux.go
@@ -51,7 +51,7 @@ func Install(dir string, ps *goolib.PkgSpec) error {
 
 // Uninstall performs a system specfic uninstall given a package extraction directory and a PkgSpec struct.
 func Uninstall(dir string, ps *client.PackageState) error {
-	un := ps.Uninstall
+	un := ps.PackageSpec.Uninstall
 	if un.Path == "" {
 		return nil
 	}
