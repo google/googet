707d6a503ead7ad3651f90215f2d861651039be4
nguyen-phillip <nguyenphillip@google.com>
2025-08-01 04:40:21 +0000

    Move database conversion code into googetdb.

--- googetdb/googetdb_test.go
+++ googetdb/googetdb_test.go
@@ -14,6 +14,7 @@ limitations under the License.
 package googetdb
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
 	"path/filepath"
@@ -23,6 +24,7 @@ import (
 	"github.com/google/go-cmp/cmp/cmpopts"
 	"github.com/google/googet/v2/client"
 	"github.com/google/googet/v2/goolib"
+	"github.com/google/googet/v2/settings"
 )
 
 func TestConvertStatetoDB(t *testing.T) {
@@ -47,7 +49,6 @@ func TestConvertStatetoDB(t *testing.T) {
 	if !cmp.Equal(s, pkgs, cmpopts.IgnoreFields(client.PackageState{}, "InstallDate")) {
 		t.Errorf("GetPackageState did not return expected result, want: %#v, got: %#v", pkgs, s)
 	}
-	os.Remove("state.db")
 }
 
 func TestRemovePackage(t *testing.T) {
@@ -74,5 +75,80 @@ func TestRemovePackage(t *testing.T) {
 		fmt.Println(diff)
 		t.Errorf("GetPackageState did not return expected result, want: %#v, got: %#v", pkgs, s)
 	}
-	os.Remove("state.db")
+}
+
+func TestCreateIfMissing(t *testing.T) {
+	for _, tc := range []struct {
+		desc      string             // description of test case
+		initialDB client.GooGetState // initial contents of db file
+		stateFile client.GooGetState // initial contents of state file
+		want      client.GooGetState // expected db contents after call
+	}{
+		{
+			desc: "no-db-but-existing-state-file",
+			stateFile: client.GooGetState{
+				client.PackageState{PackageSpec: &goolib.PkgSpec{Name: "test1"}, InstallDate: 1754021224},
+				client.PackageState{PackageSpec: &goolib.PkgSpec{Name: "test2"}, InstallDate: 1735569000},
+			},
+			want: client.GooGetState{
+				client.PackageState{PackageSpec: &goolib.PkgSpec{Name: "test1"}, InstallDate: 1754021224},
+				client.PackageState{PackageSpec: &goolib.PkgSpec{Name: "test2"}, InstallDate: 1735569000},
+			},
+		},
+		{
+			desc: "existing-db-and-state-file",
+			initialDB: client.GooGetState{
+				client.PackageState{PackageSpec: &goolib.PkgSpec{Name: "test1"}, InstallDate: 1754021224},
+				client.PackageState{PackageSpec: &goolib.PkgSpec{Name: "test2"}, InstallDate: 1735569000},
+			},
+			stateFile: client.GooGetState{
+				client.PackageState{PackageSpec: &goolib.PkgSpec{Name: "ignore3"}, InstallDate: 1625425200},
+				client.PackageState{PackageSpec: &goolib.PkgSpec{Name: "ignore4"}, InstallDate: 1698717600},
+			},
+			want: client.GooGetState{
+				client.PackageState{PackageSpec: &goolib.PkgSpec{Name: "test1"}, InstallDate: 1754021224},
+				client.PackageState{PackageSpec: &goolib.PkgSpec{Name: "test2"}, InstallDate: 1735569000},
+			},
+		},
+		{
+			desc: "no-db-and-no-state-file",
+			want: client.GooGetState{},
+		},
+	} {
+		t.Run(tc.desc, func(t *testing.T) {
+			settings.Initialize(t.TempDir(), false)
+			dbFile := settings.DBFile()
+			if len(tc.initialDB) > 0 {
+				db, err := NewDB(dbFile)
+				if err != nil {
+					t.Fatalf("NewDB(%v): %v", dbFile, err)
+				}
+				if err := db.WriteStateToDB(tc.initialDB); err != nil {
+					t.Fatalf("WriteStateToDB: %v", err)
+				}
+				db.Close()
+			}
+			if len(tc.stateFile) > 0 {
+				b, err := json.Marshal(tc.stateFile)
+				if err != nil {
+					t.Fatalf("json.Marshal(%v): %v", tc.stateFile, err)
+				}
+				os.WriteFile(settings.StateFile(), b, 0664)
+			}
+			if err := CreateIfMissing(dbFile); err != nil {
+				t.Fatalf("CreateIfMissing(%v): %v", dbFile, err)
+			}
+			db, err := NewDB(dbFile)
+			if err != nil {
+				t.Fatalf("NewDB(%v): %v", dbFile, err)
+			}
+			pkgs, err := db.FetchPkgs("")
+			if err != nil {
+				t.Fatalf("Unable to fetch packages: %v", err)
+			}
+			if diff := cmp.Diff(tc.want, pkgs, cmpopts.EquateEmpty()); diff != "" {
+				t.Fatalf("FetchPkgs got unexpected diff (-want +got):\n%v", diff)
+			}
+		})
+	}
 }
