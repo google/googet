1d4b1a6fe07ad8e87eb0d103aa701354d9dac6aa
nguyen-phillip <nguyenphillip@google.com>
2025-08-28 13:44:30 +0000

    Move GenGoo to a testutil package.

--- cli/remove/remove_test.go
+++ cli/remove/remove_test.go
@@ -1,16 +1,9 @@
 package remove
 
 import (
-	"archive/tar"
-	"compress/gzip"
-	"crypto/sha256"
-	"encoding/json"
-	"fmt"
 	"io"
-	"os"
 	"path/filepath"
 	"testing"
-	"time"
 
 	"github.com/google/go-cmp/cmp"
 	"github.com/google/go-cmp/cmp/cmpopts"
@@ -18,51 +11,9 @@ import (
 	"github.com/google/googet/v2/googetdb"
 	"github.com/google/googet/v2/goolib"
 	"github.com/google/googet/v2/settings"
+	"github.com/google/googet/v2/testutil"
 	"github.com/google/logger"
 )
-
-// genGoo creates a name.noarch.version.goo package file in directory dir for
-// the package with given pkgspec. When installed name.goo writes a file having
-// same name as the package to the dst directory. The contents of this file is
-// "name.noarch.version". Returns a RepoSpec for the goo package.
-func genGoo(t *testing.T, dir, dst string, ps goolib.PkgSpec) goolib.RepoSpec {
-	t.Helper()
-	ps.Files = map[string]string{ps.Name: filepath.Join(dst, ps.Name)}
-	b, err := json.Marshal(ps)
-	if err != nil {
-		t.Fatal(err)
-	}
-	f, err := os.Create(filepath.Join(dir, ps.String()+".goo"))
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer f.Close()
-	h := sha256.New()
-	gw := gzip.NewWriter(io.MultiWriter(h, f))
-	tw := tar.NewWriter(gw)
-	modTime := time.Now()
-	for _, x := range []struct {
-		name    string
-		content []byte
-	}{
-		{ps.Name, []byte(ps.String())},
-		{ps.Name + ".pkgspec", b},
-	} {
-		if err := tw.WriteHeader(&tar.Header{Name: x.name, Mode: 0644, Size: int64(len(x.content)), ModTime: modTime}); err != nil {
-			t.Fatal(err)
-		}
-		if _, err := tw.Write(x.content); err != nil {
-			t.Fatal(err)
-		}
-	}
-	tw.Close()
-	gw.Close()
-	return goolib.RepoSpec{
-		Checksum:    fmt.Sprintf("%x", h.Sum(nil)),
-		Source:      filepath.Base(f.Name()),
-		PackageSpec: &ps,
-	}
-}
 
 func TestRemoveOne(t *testing.T) {
 	logger.Init("GooGet", true, false, io.Discard)
@@ -128,7 +79,7 @@ func TestRemoveOne(t *testing.T) {
 			// that the remove code can find them.
 			gooDir, logDir := t.TempDir(), t.TempDir()
 			for i, ps := range tc.state {
-				rs := genGoo(t, gooDir, logDir, *ps.PackageSpec)
+				rs := testutil.GenGoo(t, gooDir, logDir, *ps.PackageSpec)
 				ps.PackageSpec = rs.PackageSpec // fixes Files
 				ps.LocalPath = filepath.Join(gooDir, ps.PackageSpec.String()+".goo")
 				ps.Checksum = rs.Checksum
